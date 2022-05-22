3b3372152853df46b651aa4a1b91d154a7eed0ae
fnaoto <f.naoto832+github@gmail.com>
2022-05-23 01:07:00 +0900

    Refactor.

--- app_member_test.go
+++ app_member_test.go
@@ -10,18 +10,18 @@ import (
 func Test_GetAppMembers(t *testing.T) {
 	t.Parallel()
 
-	r, err := recorder.New("fixtures/apps/get_app_members")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer r.Stop()
-
 	c, err := NewClient("api_token")
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	r, err := recorder.New("fixtures/apps/get_app_members")
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	c.httpClient.Transport = r
+	defer r.Stop()
 
 	resp, err := c.GetAppMembers(&AppMemberConfig{
 		Owner:    "owner",
